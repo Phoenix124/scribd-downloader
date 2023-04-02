7ea6c9527c1d7c53ca87601e402dedc3709d5323
maksimliakhavets <lyahavec.maksim@wb.ru>
2023-04-02 19:58:49 +0300

    add build.yml

--- setup.py
+++ setup.py
@@ -3,25 +3,27 @@
 from setuptools import setup, find_packages
 import os
 
-__version__ = '1.3.1'
+# __version__ comes into namespace from here
 with open(os.path.join("scribdl", "version.py")) as version_file:
     exec(version_file.read())
 
 with open("README.rst", "r") as f:
     long_description = f.read()
 
-setup(name='scribd-downloader',
+setup(name='scribd-download',
       version=__version__,
       description='Download documents, books and audiobooks off Scribd',
+      long_description='Check long description in https://github.com/Phoenix124/scribd-downloader',
+      long_description_content_type='text/x-rst',
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
-      packages=find_packages(),
+      packages = find_packages(),
       entry_points={
           'console_scripts': [
               'scribdl = scribdl.command_line:_command_line',
           ]
       },
-      url='https://github.com/Phoenix124/scribd-downloader',
+      url='https://www.github.com/ritiek/scribd-downloader',
       keywords=['scribd-downloader', 'documents', 'command-line', 'python'],
       license='MIT',
       download_url='https://github.com/ritiek/scribd-downloader/archive/v' + __version__ + '.tar.gz',
